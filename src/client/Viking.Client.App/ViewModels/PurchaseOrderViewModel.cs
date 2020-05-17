fb27b4d391478e0e63cc5e67ab4faceb890dc6ae
Justin Baur <136baur@gmail.com>
2020-05-17 14:22:12 -0400

    Added snippet for making viewmodels easier

---
+++ src/client/Viking.Client.App/ViewModels/PurchaseOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viking.Client.App.ViewModels
+{
+    public class PurchaseOrderViewModel : BindableBase
+    {
+
+		private string poNum;
+
+		public string PONum
+		{
+			get => poNum;
+			set => Set(ref poNum, value);
+		}
+    }
+}
--- src/shared/Viking.Contexts/Helpers.cs
+++ src/shared/Viking.Contexts/Helpers.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using Viking.Entities;
@@ -10,11 +11,11 @@ namespace Viking.Contexts
 {
     public static class Helpers
     {
-        public static EntityTypeBuilder<T> DefaultConfigure<T>(this EntityTypeBuilder<T> builder, System.Linq.Expressions.Expression<Func<T, object>> keyExpression)
+        public static EntityTypeBuilder<T> DefaultConfigure<T>(this EntityTypeBuilder<T> builder, Expression<Func<T, object>> keyExpression)
             where T : AuditableEntity
         {
             // Rename table to the types name
-            builder.ToTable(typeof(T).Name);
+            builder.ToTable(typeof(T).Name.ToLower());
 
             // Configure the keys they passed in
             builder.HasKey(keyExpression);
